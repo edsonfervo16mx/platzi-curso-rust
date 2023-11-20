06d02ec603b76bb08971872869f96ebb72200724
edsonfervo <edson55_@hotmail.com>
2023-11-20 12:20:52 -0500

    agregando la funcion de multiplicacion

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,30 @@ fn main() {
     println!("Hola");
     // regex
     let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+    let re_multi = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
     // input
     println!("Por favor introduce tu expresion:");
     let mut expression = String::new();
     std::io::stdin().read_line(&mut expression).unwrap();
     // operations
+    //Multiplicacion
+    loop {
+        let caps = re_multi.captures(&expression.as_str());
+
+        if caps.is_none() {
+            break;
+        }
+
+        let caps = caps.unwrap();
+
+        let cap_expression = caps.get(0).unwrap().as_str();
+
+        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+        let multi = left_value * right_value;
+        expression = expression.replace(cap_expression, &multi.to_string());
+    }
+    //Suma
     loop {
         let caps = re_add.captures(&expression.as_str());
 
