457c967f8aae0ed9bb1e6a64380de24b2ebe5fe4
edsonfervo <edson55_@hotmail.com>
2023-11-25 13:54:15 -0500

    depurando

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,6 @@ fn main() {
 
         names.push(name);
     }
-    // println!("Por favor introduce un nombre: ");
-    // let mut name = String::new();
-    // std::io::stdin().read_line(&mut name).unwrap();
-
-    // names.push(name);
-
     println!("Los nombres introducidos son: {:?}", names);
 
     for nombre in names {
