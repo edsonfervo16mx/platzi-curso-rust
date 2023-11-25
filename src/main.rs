f33d91ff135062aa3e112a7ba461f275145c50d1
edsonfervo <edson55_@hotmail.com>
2023-11-25 14:54:57 -0500

    generador de folios con ceros

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 fn main() {
-    let mut names: Vec<String> = Vec::new();
+    println!("Ingresar un numero:");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
 
-    for _ in 0..5 {
-        println!("Por favor introduce un nombre: ");
-        let mut name = String::new();
-        std::io::stdin().read_line(&mut name).unwrap();
+    println!("Ingresar el numero de ceros");
+    let mut ceros = String::new();
+    std::io::stdin().read_line(&mut ceros).unwrap();
 
-        names.push(name);
-    }
-    println!("Los nombres introducidos son: {:?}", names);
+    generate_folio(
+        input.trim().parse::<i32>().unwrap(),
+        ceros.trim().parse::<i32>().unwrap(),
+    );
 
-    for nombre in names {
-        println!("{}", nombre);
-    }
+    test_format();
+}
+
+fn generate_folio(n: i32, ceros: i32) {
+    let formatted_folio = format!("{:0width$}", n, width = ceros as usize);
+
+    println!("El folio es: {}", formatted_folio);
+}
+
+fn test_format() {
+    let string = format!("{:0width$}", 1, width = 5);
+    // 00001
+    println!("{}", string);
 }
